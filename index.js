33a0ac19510d9619c74ad1ea778451d9df7ab4b7
github-cloudlabsuser-718 <167429600+github-cloudlabsuser-718@users.noreply.github.com>
2024-04-24 15:51:03 +0000

    Copilot commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const MarkdownEditor = () => {
+    const [markdown, setMarkdown] = useState('type markdown here');
+
+    const handleMarkdownChange = (event) => {
+        setMarkdown(event.target.value);
+    };
+
+    return (
+        <div>
+            <textarea value={markdown} onChange={handleMarkdownChange} />
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+        </div>
+    );
+};
+
+export default MarkdownEditor;
